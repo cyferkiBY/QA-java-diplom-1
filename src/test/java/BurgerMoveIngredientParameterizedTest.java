5a15cff63b286965ecf669480a9a73d315363fb3
Kate Bay <kate.by.2022@yandex.ru>
2022-05-20 21:54:08 +0200

    Fixed BurgerMoveIngredientParameterizedTest: Removed try/catch and made an exception check in a separate method.

--- src/test/java/BurgerMoveIngredientParameterizedTest.java
+++ src/test/java/BurgerMoveIngredientParameterizedTest.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,28 +16,32 @@ import static org.junit.Assert.assertEquals;
 @RunWith(Parameterized.class)
 public class BurgerMoveIngredientParameterizedTest {
 
+    private enum TypeOfTheTest{EXCEPTION, NOT_EXCEPTION};
+
     static private final Ingredient INGREDIENT_FIRST = new Ingredient(IngredientType.FILLING, "Filling ingredient", 5);
     static private final Ingredient INGREDIENT_SECOND = new Ingredient(IngredientType.SAUCE, "Sauce ingredient", 4);
     static private final Ingredient INGREDIENT_THIRD = new Ingredient(IngredientType.SAUCE, "New sauce ingredient", 6);
     private final int index;
     private final int newIndex;
     private final List<Ingredient> expectedIngredientsList;
+    private final TypeOfTheTest typeOfTheTest;
     private Burger burger;
 
-    public BurgerMoveIngredientParameterizedTest(int index, int newIndex, List<Ingredient> expectedIngredientsList) {
+    public BurgerMoveIngredientParameterizedTest(int index, int newIndex, List<Ingredient> expectedIngredientsList, TypeOfTheTest typeOfTheTest) {
         this.index = index;
         this.newIndex = newIndex;
         this.expectedIngredientsList = expectedIngredientsList;
+        this.typeOfTheTest = typeOfTheTest;
     }
 
     @Parameterized.Parameters(name = "{index}: \"index\": {0}; \"newIndex\": {1};")
     public static Object[][] getIndexData() {
         return new Object[][]{
-                {0, 2, Arrays.asList(INGREDIENT_SECOND, INGREDIENT_THIRD, INGREDIENT_FIRST)},
-                {2, 0, Arrays.asList(INGREDIENT_THIRD, INGREDIENT_FIRST, INGREDIENT_SECOND)},
-                {0, 0, Arrays.asList(INGREDIENT_FIRST, INGREDIENT_SECOND, INGREDIENT_THIRD)},
-                {0, 4, Arrays.asList(INGREDIENT_FIRST, INGREDIENT_SECOND, INGREDIENT_THIRD)},
-                {4, 0, Arrays.asList(INGREDIENT_FIRST, INGREDIENT_SECOND, INGREDIENT_THIRD)},
+                {0, 2, Arrays.asList(INGREDIENT_SECOND, INGREDIENT_THIRD, INGREDIENT_FIRST), TypeOfTheTest.NOT_EXCEPTION},
+                {2, 0, Arrays.asList(INGREDIENT_THIRD, INGREDIENT_FIRST, INGREDIENT_SECOND), TypeOfTheTest.NOT_EXCEPTION},
+                {0, 0, Arrays.asList(INGREDIENT_FIRST, INGREDIENT_SECOND, INGREDIENT_THIRD), TypeOfTheTest.NOT_EXCEPTION},
+                {0, 4, Arrays.asList(INGREDIENT_FIRST, INGREDIENT_SECOND, INGREDIENT_THIRD), TypeOfTheTest.EXCEPTION},
+                {4, 0, Arrays.asList(INGREDIENT_FIRST, INGREDIENT_SECOND, INGREDIENT_THIRD), TypeOfTheTest.EXCEPTION},
         };
     }
 
@@ -48,13 +54,17 @@ public class BurgerMoveIngredientParameterizedTest {
     }
 
     @Test
-    public void checkMoveIngredient() {
-        try {
-            burger.moveIngredient(index, newIndex);
-            assertEquals("Список ингредиентов не совпал с ожидаемым", expectedIngredientsList, burger.ingredients);
-        } catch (IndexOutOfBoundsException thrown) {
-            assertEquals("Произошло исключение IndexOutOfBoundsException", "", thrown.getMessage());
-        }
+    public void checkMoveIngredientWithValidIndex() {
+        Assume.assumeTrue(typeOfTheTest == TypeOfTheTest.NOT_EXCEPTION);
+        burger.moveIngredient(index, newIndex);
+        assertEquals("Список ингредиентов не совпал с ожидаемым", expectedIngredientsList, burger.ingredients);
+    }
+
+    @Test
+    public void checkMoveIngredientWithNotValidIndex() {
+        Assume.assumeTrue(typeOfTheTest == TypeOfTheTest.EXCEPTION);
+        //предположила, что исключение это ожидаемое и корректное поведение метода при вводе некорректного индекса
+        Assert.assertThrows("Не вызвано ожидаемое исключение IndexOutOfBoundsException", IndexOutOfBoundsException.class, () -> {burger.moveIngredient(index, newIndex);});
     }
 
 }
